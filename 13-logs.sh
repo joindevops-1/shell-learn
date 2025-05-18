a0615dbe8690a02ea5a20c94af8ced216bff916c
msivakumarreddy <info@joindevops.com>
2025-05-18 11:09:25 +0530

    hello world started

--- 13-logs.sh
+++ 13-logs.sh
@@ -9,7 +9,7 @@ LOGS_FOLDER=/var/log/shellscript-logs
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOG_FILE
+mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $date" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]
